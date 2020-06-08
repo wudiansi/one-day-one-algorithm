087cfc75031b5b927f468698c6a78952b7ee20f1
wuds <wuds@cume.cc>
2020-06-08 15:50:54 +0800

    2.rob

--- 2-rob.js
+++ 2-rob.js
@@ -1,9 +1,9 @@
 function rob(nums) {  
   let len = nums.length;
   if(len === 0) return 0; // 空数组
-  if(len === 1) dp[1] = nums[0];
-  if(len === 2) dp[2] = Math.max(nums[0], nums[1]);
-  if(len === 3) dp[3] = Math.max(nums[1], nums[0] + nums[2]);
+  if(len === 1) return nums[0];
+  if(len === 2) return Math.max(nums[0], nums[1]);
+  if(len === 3) return Math.max(nums[1], nums[0] + nums[2]);
   let dp = [nums[0], Math.max(nums[0], nums[1]), Math.max(nums[1], nums[0] + nums[2])];
 
   for(var i = 3; i < len; i++ ) {
