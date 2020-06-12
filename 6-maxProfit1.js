9a09c82303144732d0a074d1ea12b57ef44cab90
wuds <wuds@cume.cc>
2020-06-12 11:07:23 +0800

    6.maxProfit 2

--- 6-maxProfit1.js
+++ 6-maxProfit1.js
@@ -3,7 +3,7 @@
 * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
 * 注意：你不能在买入股票前卖出股票
  */
-function maxProfit(prices){
+function maxProfit1(prices){
   // if(prices.length <= 1) return 0
   // let diff = [];
   // for(let i = 0; i < prices.length - 1; i++) {
@@ -27,5 +27,18 @@ function maxProfit(prices){
 
   return max
 }
+// console.log(maxProfit1([7,1,5,3,6,4]));
 
-console.log(maxProfit([7,1,5,3,6,4]));
+/**
+* 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+* 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+* 注意：你不能在买入股票前卖出股票(你必须在再次购买前出售掉之前的股票)
+ */
+function maxProfit2(prices){
+  let sum = 0;
+  for(let i = 0; i < prices.length - 1; i++ ){
+    if(prices[i+1] > prices[i]) sum += prices[i+1] - prices[i]
+  }
+
+  return sum
+}
