5de5fbaa95af787f47c91514047ca5fafa8c0b06
wuds <wuds@cume.cc>
2020-07-01 09:48:41 +0800

    19.permute

---
+++ 19-permute.js
@@ -0,0 +1,20 @@
+function permute(nums) {
+  let res = [];
+  let len = nums.length;
+
+  function back(path) {
+    if(path.length === nums.length){
+      return res.push([...path])
+    }
+    for(let i = 0; i < len; i++){
+      if(path.indexOf(nums[i]) === -1){
+        path.push(nums[i]);
+        back(path);
+        path.pop();
+      }
+    }
+  }
+  back([]);
+
+  return res;
+}
