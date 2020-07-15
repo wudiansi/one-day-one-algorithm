0075ae4405c7c85ae1648fa98814bdb95e431720
wuds <wuds@cume.cc>
2020-07-15 09:57:28 +0800

    28.heapSort

---
+++ 28-heapSort.js
@@ -0,0 +1,31 @@
+function adjustMaxHeap(heap,head,heapSize){
+  let temp = heap[head];
+  let child = head * 2 + 1;
+
+  while(child < heapSize){
+    if(child+1 < heapSize && heap[child] < heap[child+1]) { child++ };
+    if(heap[head] < heap[child]){
+      heap[head] = heap[child];
+      head = child;
+      child = head * 2 + 1;
+    }else break;
+    heap[head] = temp;
+  }
+}
+
+function buildHeap(heap){
+  for(let i = (heap.length-1) >> 1;i >= 0;i--){
+    adjustMaxHeap(heap, i, heap.length);
+  }
+}
+
+function heapSort(arr){
+  buildHeap(arr);
+  for(let i = arr.length-1;i > 0;i--){
+    [arr[i],arr[0]] = [arr[0],arr[i]];
+    adjustMaxHeap(arr, 0, i);
+  }
+  return arr;
+}
+
+console.log(heapSort([6,5,3,1,8,7,2,4]));
