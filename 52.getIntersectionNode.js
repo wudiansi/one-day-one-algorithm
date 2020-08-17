482f5273e8b4cd16a5603866b8012a8d7260cd08
wuds <wuds@cume.cc>
2020-08-17 09:48:09 +0800

    52.getInterSectionNode

---
+++ 52.getIntersectionNode.js
@@ -0,0 +1,10 @@
+getInterSectionNode(headA, headB) {
+  let p1 = headA, p2 = headB;
+  if(p1 == null || p2 == null) return null
+
+  while(p1 !== p2) {
+    p1 = p1 === null ? headB : p1.next;
+    p2 = p2 === null ? heada : p2.next;
+  }
+  return p1
+}
